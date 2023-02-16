73cc83067c77b1d02e834c6f9152b77f0a3b83bd
Dan Kiuna <dkkiuna11@gmail.com>
2023-02-16 16:56:03 -0600

    minor routing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InComponent } from './in/in.component';
+import { OutComponent } from './out/out.component';
+import { RouterModule, Routes } from '@angular/router'
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
+
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'in', component: InComponent },
+  { path: 'out', component: OutComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InComponent,
+    OutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
