d7a9c97b8ae33aacdaf3074fc5e4df79d2942d69
Dan Kiuna <dkkiuna11@gmail.com>
2023-02-16 14:04:23 -0600

    increase budget size

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-
 
 const updatedConfig = {
   ...aws_exports,
   oauth: {
     ...aws_exports.oauth,
-    redirectSignIn: aws_exports.oauth.redirectSignIn.split(",")[0],
+    redirectSignIn: aws_exports.oauth.redirectSignIn.split(",")[1],
     redirectSignOut: aws_exports.oauth.redirectSignOut.split(",")[0],
   },
 };
 
 Amplify.configure(updatedConfig);
+
+console.log('======>',Auth.configure())
 
 if (environment.production) {
   enableProdMode();
